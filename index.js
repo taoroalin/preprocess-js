c58b9a3cb8d500ecc471c867734623204fbd8fff
Tao Lin <taoroalin@gmail.com>
2021-02-05 01:34:36 -0500

    real initial commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const minifyHTML = (text) => {
-  return text.replace(/>\w+</, "><")
+  return text.replace(/>\s+</, "><").replace(/\s{2,}/, " ")
+}
+
+const minifyCSS = (text) => {
+  return text.replace(/;\s+/, ";").replace(/\{\s+/, "{").replace(/\}\s+/, "}");
+}
+
+const preprocessTemplates = (htmlText) => {
+  return htmlText;
 }
